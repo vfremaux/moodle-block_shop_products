3393928e55a35f482466cd9f96e1222ae831f7e2
valery <valery.fremaux@gmail.com>
2016-09-26 10:40:17 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -15,22 +15,22 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * Version details.
  *
- * @package    block_shop_products
- * @category   blocks
- * @author     Valery Fremaux <valery.fremaux@gmail.com>
- * @copyright  2016 onwards Valery Fremaux <valery.fremaux@gmail.com>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     block_shop_products
+ * @category    blocks
+ * @author      Valery Fremaux <valery.fremaux@gmail.com>
+ * @copyright   2016 onwards Valery Fremaux (http://www.mylearningfactory.com)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016060500;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2016051900;        // Requires this Moodle version
-$plugin->component = 'block_shop_products'; // Full name of the plugin (used for diagnostics)
-$plugin->maturity = MATURITY_BETA; // Full name of the plugin (used for diagnostics)
-$plugin->release = '3.1.0 (Build 2016060500)'; // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2016060500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2016051900;        // Requires this Moodle version.
+$plugin->component = 'block_shop_products'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '3.1.0 (Build 2016060500)';
 
 // Non moodle attributes.
 $plugin->codeincrement = '3.1.0000';
